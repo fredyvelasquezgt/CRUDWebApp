57d376237ae4572d41d1744479c8189701af730e
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-17 17:24:58 -0600

    tasks listed

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,17 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
+              {
+                tareas.map(item => (
+          <li className="list-group-item" key={item.id}>
+              <span className="lead">{item.NombreTarea}</span>
               <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
               <button className="btn btn-warning btn-sm float-end">Editar</button>
             </li>
+                ))
+              }
+            
+
           </ul>
         </div>
         <div className="col-4">
